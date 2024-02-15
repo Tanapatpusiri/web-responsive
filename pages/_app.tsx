45ebde7045b4cd28e02641f5673ed41677fea3dc
Tanapatpusiri <88122903+Tanapatpusiri@users.noreply.github.com>
2024-02-15 12:30:38 +0700

    fix: section1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { ThemeProvider } from "@emotion/react";
+import  theme from "../styles/theme";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider> ;
 }

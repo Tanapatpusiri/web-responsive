4c3acf9c23532d7c06ada84c226f3c0c46e3e5f3
Tanapatpusiri <88122903+Tanapatpusiri@users.noreply.github.com>
2024-02-19 09:55:15 +0700

    fix: section1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,9 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import  theme from "../styles/theme";
 export default function App({ Component, pageProps }: AppProps) {
-  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider> ;
+  return (
+    <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+     </ThemeProvider>
+)
 }
